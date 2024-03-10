201ef8b44ef894b266810ccafe969f015a88da29
SafarSoFar <133248217+SafarSoFar@users.noreply.github.com>
2024-03-10 15:49:41 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace B64_GUI_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            encButton = new Button();
+            decButton = new Button();
+            clearButton = new Button();
+            dragList = new ListBox();
+            SuspendLayout();
+            // 
+            // encButton
+            // 
+            encButton.Location = new Point(483, 32);
+            encButton.Name = "encButton";
+            encButton.Size = new Size(139, 102);
+            encButton.TabIndex = 1;
+            encButton.Text = "Encrypt";
+            encButton.UseVisualStyleBackColor = true;
+            encButton.Click += encButton_Click;
+            // 
+            // decButton
+            // 
+            decButton.Location = new Point(483, 158);
+            decButton.Name = "decButton";
+            decButton.Size = new Size(139, 102);
+            decButton.TabIndex = 2;
+            decButton.Text = "Decrypt";
+            decButton.UseVisualStyleBackColor = true;
+            decButton.Click += decButton_Click;
+            // 
+            // clearButton
+            // 
+            clearButton.Location = new Point(12, 287);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(610, 43);
+            clearButton.TabIndex = 3;
+            clearButton.Text = "Clear the box";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
+            // dragList
+            // 
+            dragList.AllowDrop = true;
+            dragList.FormattingEnabled = true;
+            dragList.Location = new Point(12, 32);
+            dragList.Name = "dragList";
+            dragList.Size = new Size(426, 244);
+            dragList.TabIndex = 4;
+            dragList.DragDrop += dragList_DragDrop;
+            dragList.DragEnter += dragList_DragEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(661, 342);
+            Controls.Add(dragList);
+            Controls.Add(clearButton);
+            Controls.Add(decButton);
+            Controls.Add(encButton);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Base64 Application";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button encButton;
+        private Button decButton;
+        private Button clearButton;
+        private ListBox dragList;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System.Drawing;
+using System.Text;
+
+namespace B64_GUI_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool CheckItemsCount()
+        {
+
+            if (dragList.Items.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void encButton_Click(object sender, EventArgs e)
+        {
+            if(!CheckItemsCount())
+            {
+                MessageBox.Show("Drag the files to encode", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int len = dragList.Items.Count;
+            //MessageBox.Show(len.ToString());
+            for (int i = 0; i < len; i++)
+            {
+                string filePath = dragList.Items[i].ToString();
+                Byte[] plainBytes = File.ReadAllBytes(filePath);
+                String b64Code = Convert.ToBase64String(plainBytes);
+
+                Byte[] codeBytes = Encoding.UTF8.GetBytes(b64Code);
+                File.WriteAllBytes(filePath, codeBytes);
+
+            }
+            MessageBox.Show("The files are encoded");
+            ClearList();
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            ClearList();
+        }
+
+        private void ClearList()
+        {
+            dragList.Items.Clear();
+
+        }
+
+        private void dragList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void dragList_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] data = ((string[])e.Data.GetData(DataFormats.FileDrop, false));
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                dragList.Items.Add(data[i]);
+            }
+        }
+
+        private void decButton_Click(object sender, EventArgs e)
+        {
+            if (!CheckItemsCount())
+            {
+                MessageBox.Show("Drag the files to decode", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int len = dragList.Items.Count;
+            for (int i = 0; i < len; i++)
+            {
+                try
+                {
+                    string filePath = dragList.Items[i].ToString();
+    
+                    String strCode = File.ReadAllText(filePath);
+                    Byte[] codeBytes = Convert.FromBase64String(strCode);
+                    //String b64Plain = BitConverter.ToString(codeBytes, 0, codeBytes.Length);
+
+                    File.WriteAllBytes(filePath, codeBytes);
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("The file is not base64 encrypted","Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            MessageBox.Show("The files are decoded");
+            ClearList();
+        }
+    }
+}
